32ffc3df940c55b5be85da5fcafda989ea710b13
AbhinavTiruvee <ranjaniabhinav@gmail.com>
2023-10-02 14:49:05 -0500

    deploy

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://AbhinavTiruvee.github.io',
-  title: 'AT.',
+  title: 'AT',
 }
 
 const about = {
